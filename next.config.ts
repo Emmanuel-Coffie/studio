578166372f242de7034f41b5a34fb6d3ff74ae5a
Emmanuel Coffie <coffie09emmanuel@gmail.com>
2025-11-12 14:10:05 +0000

    i dont think the image was added to the file...here is the the image i w

--- next.config.ts
+++ next.config.ts
@@ -24,12 +24,6 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
         hostname: 'storage.googleapis.com',
         port: '',
         pathname: '/**',
